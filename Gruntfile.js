2e506bcfe475e4efc66b7290251e27c6be317073
Alain Boudard <aboudard@oxiane.com>
2014-03-25 10:46:55 +0100

    Added shell android build
    Added a cordova shell command for build.
    Added /res clean command.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt){
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('Package.json'),
-        clean: ["platforms/android/assets/www/scss"],
+        clean: ["platforms/android/assets/www/scss","platforms/android/assets/www/res"],
         copy: {
 			main: {
 				files: [
@@ -11,22 +11,29 @@ module.exports = function(grunt){
 				  {src: 'www/res/icon/android/icon-48-mdpi.png', dest: 'platforms/android/res/drawable-mdpi/icon.png'},
 				  {src: 'www/res/icon/android/icon-48-mdpi.png', dest: 'platforms/android/res/drawable/icon.png'},
 				  {src: 'www/res/icon/android/icon-72-hdpi.png', dest: 'platforms/android/res/drawable-hdpi/icon.png'},
-				  {src: 'www/res/icon/android/icon-96-xhdpi.png', dest: 'platforms/android/res/drawable-xhdpi/icon.png'}
+				  {src: 'www/res/icon/android/icon-96-xhdpi.png', dest: 'platforms/android/res/drawable-xhdpi/icon.png'},
+				  {src: 'www/res/icon/android/icon-144-xxhdpi.png', dest: 'platforms/android/res/drawable-xxhdpi/icon.png'}
 				]
 			}
 		},
 		sass: {
 		    dist: {
-		      files: {
-		        'platforms/android/assets/www/css/app.css' : 'www/scss/app.scss'
-		      }
-		    }
-		  }
+		    	files: {
+		        	'platforms/android/assets/www/css/app.css' : 'www/scss/app.scss'
+		    	}
+			}
+		},
+		shell: {
+	        makeDir: {
+	            command: 'cordova build android'
+	        }
+	    }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.registerTask('default', ['clean','copy','sass']);
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.registerTask('default', ['sass','shell','clean','copy']);
 
 };
